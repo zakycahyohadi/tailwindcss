9968ef5887c563d9279b4104e01a1502bc238e23
Apple <apple@Apple-Mac.local>
2024-01-23 10:32:32 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,55 @@
 //navbar fixed
-window.onscroll = function(){
+window.onscroll = function () {
     const header = document.querySelector('header');
     const navbarFixed = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
-    if (window.pageYOffset > navbarFixed){
+    if (window.pageYOffset > navbarFixed) {
         header.classList.add('navbar-fixed');
-    }else{
+        toTop.classList.remove('hidden')
+    } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden')
+
     }
 }
 
 // button humberger
-const humberger =document.querySelector('#humberger');
-const navMenu =document.querySelector('#nav-menu');
+const humberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu');
 
-humberger.addEventListener('click',function(){
+humberger.addEventListener('click', function () {
     humberger.classList.toggle('active');
     navMenu.classList.toggle('hidden');
-})
+});
+
+// klik dimanapun
+window.addEventListener('click', function (a) {
+    if (a.target != humberger && a.target != navMenu) {
+        humberger.classList.remove('active')
+        navMenu.classList.add('hidden')
+    }
+});
+
+//  darkmode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+
+// perpindahan tombol
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
